75df1d44553d22c5d30eaec76168906221d65dc9
bulutyurekliadam <ayvaz.hakan@outlook.com>
2023-12-12 01:28:06 +0300

    Add project files.

---
+++ MyMongoDbAjaxProject/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MyMongoDbAjaxProject.DAL.Entities;
+using MyMongoDbAjaxProject.DAL.Settings;
+
+namespace MyMongoDbAjaxProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IMongoCollection<Category> _categoryCollection;
+        public CategoryController(IDataBaseSettings _dataBaseSettings)
+        {
+            var client = new MongoClient(_dataBaseSettings.ConnectionString);
+            var database = client.GetDatabase(_dataBaseSettings.DatabaseName);
+            _categoryCollection = database.GetCollection<Category>(_dataBaseSettings.CategoryCollectionName);
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyMongoDbAjaxProject/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MyMongoDbAjaxProject.DAL.Entities;
+using MyMongoDbAjaxProject.DAL.Settings;
+
+namespace MyMongoDbAjaxProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IMongoCollection<Product> _productCollection;
+        public ProductController(IDataBaseSettings _dataBaseSettings)
+        {
+            var client = new MongoClient(_dataBaseSettings.ConnectionString);
+            var database = client.GetDatabase(_dataBaseSettings.DatabaseName);
+            _productCollection = database.GetCollection<Product>(_dataBaseSettings.ProductCollectionName);
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
